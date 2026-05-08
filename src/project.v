db47831b2216464add85dcee9b4df1d8245e9113
Nicholas <96741837+28add11@users.noreply.github.com>
2026-05-08 12:57:53 -0600

    Update project.v to test area use

--- src/project.v
+++ src/project.v
@@ -6,7 +6,7 @@
 
 `default_nettype none
 
-module tt_um_28add11_latchup(
+module tt_um_vga_example(
   input  wire [7:0] ui_in,    // Dedicated inputs
   output wire [7:0] uo_out,   // Dedicated outputs
   input  wire [7:0] uio_in,   // IOs: Input path
@@ -16,76 +16,23 @@ module tt_um_28add11_latchup(
   input  wire       clk,      // clock
   input  wire       rst_n     // reset_n - low to reset
 );
-  // increase couner every frame (vsync happens once per frame)
-  reg [9:0] counter;
+
+  // X color bands shift register
+  reg [79:0] xShiftReg;
+  wire randBit;
+  assign randBit = ((xShiftReg[79] ~^ xShiftReg[78]) ~^ xShiftReg[42]) ~^ xShiftReg[41]; // Tap placement from https://docs.amd.com/v/u/en-US/xapp052
   always @(posedge vsync, negedge rst_n) begin
     if (~rst_n) begin
-      counter <= 0;
+		// Fun magic number ;)
+      xShiftReg <= {72'h6C6F766569743F00, ui_in}; // Seed with custom value if you want
     end else begin
-      counter <= counter + 1;
+      xShiftReg <= {xShiftReg[78:0], randBit};
     end
-  end  
+  end 
 
-
-  // adapted from https://github.com/edrosten/8bit_rng
-  reg [7:0] a;
-  reg [7:0] x;
-  reg [7:0] y;
-  reg [7:0] z;
-  wire [7:0] t;
-  assign t = x ^ (x << 4);
-  always @(posedge clk, negedge rst_n) begin
-    if (~rst_n) begin
-      a <= 1;
-      x <= 0;
-      y <= 0;
-      z <= 0;
-    end else begin
-      x <=y ;
-		  y <=z ;
-		  z <=a ;
-      a <= z ^ t ^ ( z >> 1) ^ (t << 1);
-    end
-  end
-
-  // animate layers
-  wire [9:0] layer_a_x = counter[5] ? (pix_x - counter*16) : (pix_x + counter*16);
-  wire [9:0] layer_a_y = counter[8] ? (pix_y - counter*2) : (pix_y + counter*2);
-
-  wire [9:0] layer_b_x = pix_x + counter*7;
-  wire [9:0] layer_b_y = pix_y + counter + counter/2;
-
-  wire [9:0] layer_c_x = pix_x + counter*4;
-  wire [9:0] layer_c_y = pix_y + counter/2;
-
-  wire [9:0] layer_d_x = pix_x + counter*2;
-  wire [9:0] layer_d_y = pix_y + counter/4;
-
-  wire [9:0] layer_e_x = pix_x + counter/2;
-  wire [9:0] layer_e_y = pix_y + counter/6;
-
-  //                    checker shape          * transparency using pixel dithering
-  wire layer_a = (layer_a_x[8] ^ layer_a_y[8]) & (a[0]);
-  wire layer_b = (layer_b_x[7] ^ layer_b_y[7]) & (~pix_y[0] ^ pix_x[1]);
-  wire layer_c =  layer_c_x[6] ^ layer_c_y[6] ;
-  wire layer_d =  layer_d_x[5] ^ layer_d_y[5] ;
-  wire layer_e = (layer_e_x[4] ^ layer_e_y[4]) & ( pix_y[1] ^ pix_x[0]);
-
-  wire [5:0] color_a = ~ui_in[5:0]; // color of the closest layer
-  wire [5:0] color_b = color_a ^ 6'b00_10_10;
-  wire [5:0] color_c = color_b & 6'b10_10_10;
-  wire [5:0] color_de = color_b & 6'b11_01_01; // color of the two farthest layers
-                                      // the layer e also using dithering to darken the color
-
-  assign {R, G, B} =
-      video_active ?
-        (layer_a ? a[5:0] :
-          (layer_b ? color_b : 
-            (layer_c ? color_c : 
-              (layer_d ? color_de :
-                (layer_e ? color_de : 6'b00_00_00))))) : 6'b00_00_00;
-
-  
+  wire [1:0] green;
+  assign green = xShiftReg[pix_x[9:3] -: 2];
+  assign {R, G, B} = (green == 2'b00) ? {2'b01, 2'b10, 2'b00} : {2'b00, green, 2'b00};
 
   // VGA signals
   wire hsync;
